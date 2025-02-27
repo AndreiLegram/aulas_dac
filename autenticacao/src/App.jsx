22a2830111efacdcb5e8c48c3ecaadd4ba85644b
Andrei Legramante <andrei.050103@bento.local>
2025-02-27 20:07:53 -0300

    update 27022025

---
+++ autenticacao/src/App.jsx
@@ -0,0 +1,12 @@
+import AuthProvider from "./context/AuthProvider"; // Importa o AuthProvider
+import Header from "./components/Header";
+import LoginPage from "./components/LoginPage";
+function App() {
+  return (
+    <AuthProvider>
+      <Header />
+      <LoginPage />
+    </AuthProvider>
+  );
+}
+export default App;
---
+++ autenticacao/src/context/AuthContext.jsx
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+// Criando o contexto de autenticação
+const AuthContext = createContext();
+export default AuthContext;
